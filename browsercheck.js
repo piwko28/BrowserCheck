b5610a73a142b6f2a96f093456ce602fb6213646
Szymon Piwowarczyk <szymon.piwowarczyk@sienn.pl>
2015-07-17 12:17:31 +0200

    separated browsers to browsers and devices

--- browsercheck.js
+++ browsercheck.js
@@ -1,19 +1,39 @@
 /* BROWSER */
 jQuery.browser = {};
-jQuery.browser.android = /android/i.test(navigator.userAgent.toLowerCase());
-jQuery.browser.blackBerry = /blackberry/i.test(navigator.userAgent.toLowerCase());
-jQuery.browser.webOs = /webos/i.test(navigator.userAgent.toLowerCase());
-jQuery.browser.windowsPhone = /windows phone/i.test(navigator.userAgent.toLowerCase());
-jQuery.browser.iDevice = /ipad|iphone|ipod/i.test(navigator.userAgent.toLowerCase());
-jQuery.browser.iPad = /ipad/i.test(navigator.userAgent.toLowerCase());
-jQuery.browser.iPhone = /iphone/i.test(navigator.userAgent.toLowerCase());
-jQuery.browser.iPod = /ipod/i.test(navigator.userAgent.toLowerCase());
-jQuery.browser.mobile = jQuery.browser.android ||
-                        jQuery.browser.blackBerry ||
-                        jQuery.browser.webOs ||
-                        jQuery.browser.windowsPhone ||
-                        jQuery.browser.iDevice;
-jQuery.browser.nonMobile = !jQuery.browser.mobile;
+jQuery.browser.chrome = /chrom(e|ium)/.test(navigator.userAgent.toLowerCase()) && !(/opr/.test(navigator.userAgent.toLowerCase()));
+jQuery.browser.opera = /(opera|opr)/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.ie7 = /msie 7/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.ie8 = /msie 8/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.ie9 = /msie 9/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.ie10 = /msie 10/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.ie11 = /trident.*rv:11/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.ie = jQuery.browser.ie7 ||
+                     jQuery.browser.ie8 ||
+                     jQuery.browser.ie9 ||
+                     jQuery.browser.ie10 ||
+                     jQuery.browser.ie11;
+jQuery.browser.ff = /mozilla/.test(navigator.userAgent.toLowerCase()) &&
+											!(/(compatible)/.test(navigator.userAgent.toLowerCase())) &&
+											!(jQuery.browser.ie ||
+											jQuery.browser.chrome ||
+											jQuery.browser.opera);
+
+/* DEVICES */
+jQuery.device = {};
+jQuery.device.android = /android/i.test(navigator.userAgent.toLowerCase());
+jQuery.device.blackBerry = /blackberry/i.test(navigator.userAgent.toLowerCase());
+jQuery.device.webOs = /webos/i.test(navigator.userAgent.toLowerCase());
+jQuery.device.windowsPhone = /windows phone/i.test(navigator.userAgent.toLowerCase());
+jQuery.device.iDevice = /ipad|iphone|ipod/i.test(navigator.userAgent.toLowerCase());
+jQuery.device.iPad = /ipad/i.test(navigator.userAgent.toLowerCase());
+jQuery.device.iPhone = /iphone/i.test(navigator.userAgent.toLowerCase());
+jQuery.device.iPod = /ipod/i.test(navigator.userAgent.toLowerCase());
+jQuery.device.mobile = jQuery.device.android ||
+                        jQuery.device.blackBerry ||
+                        jQuery.device.webOs ||
+                        jQuery.device.windowsPhone ||
+                        jQuery.device.iDevice;
+jQuery.device.nonMobile = !jQuery.device.mobile;
 
 /* PLATFORM */
 jQuery.platform = {};
