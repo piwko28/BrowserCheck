b353bc7224c74defddaef3f34508b8bd55893259
Szymon Piwowarczyk <szymon.piwowarczyk@sienn.pl>
2015-07-17 12:18:26 +0200

    Possibility to add classes to "html" tag to depends styles on browser/device type (not only js).

--- browsercheck.js
+++ browsercheck.js
@@ -45,3 +45,36 @@ jQuery.platform.unknown = !(jQuery.platform.windows ||
                             jQuery.platform.macOs || 
                             jQuery.platform.unix || 
                             jQuery.platform.linux);
+
+function envoronmentDetect(browser, device, os) {
+  var html = $("html");
+  if(browser) {
+    if(jQuery.browser.chrome) html.addClass('chrome');
+    if(jQuery.browser.opera) html.addClass('opera');
+    if(jQuery.browser.ff) html.addClass('ff');
+    if(jQuery.browser.ie7) html.addClass('ie7');
+    if(jQuery.browser.ie8) html.addClass('ie8');
+    if(jQuery.browser.ie9) html.addClass('ie9');
+    if(jQuery.browser.ie10) html.addClass('ie10');
+    if(jQuery.browser.ie11) html.addClass('ie11');
+    if(jQuery.browser.ie) html.addClass('ie');
+  }
+  if(device) {
+    if(jQuery.device.android) html.addClass('android');
+    if(jQuery.device.blackBerry) html.addClass('blackberry');
+    if(jQuery.device.webOs) html.addClass('webos');
+    if(jQuery.device.windowsPhone) html.addClass('windowsphone');
+    if(jQuery.device.iDevice) html.addClass('idevice');
+    if(jQuery.device.iPad) html.addClass('ipad');
+    if(jQuery.device.iPhone) html.addClass('iphone');
+    if(jQuery.device.iPod) html.addClass('ipod');
+    if(jQuery.device.mobile) html.addClass('mobile');
+    if(jQuery.device.nonMobile) html.addClass('nonmobile');
+  }
+  if(os) {
+    if(jQuery.platform.windows) html.addClass('windows');
+    if(jQuery.platform.macOs) html.addClass('macos');
+    if(jQuery.platform.unix) html.addClass('unix');
+    if(jQuery.platform.linux) html.addClass('linux');
+  }
+}
